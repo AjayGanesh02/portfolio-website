85b05f7668e9930acf44db3b085f7939e88fd6f8
Ajay Ganesh <ajganesh2002@gmail.com>
2022-03-31 16:27:59 -0400

    Hamburger and Side Menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -3,9 +3,9 @@ import CodeIcon from '@mui/icons-material/Code';
 import PhoneIcon from '@mui/icons-material/Phone';
 import MailIcon from '@mui/icons-material/Mail';
 
-export default function Topbar() {
+export default function Topbar({ menuOpen, setMenuOpen }) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")}>
       <div className="wrapper">
         <div className="left">
           <CodeIcon className="codelogo" />
@@ -20,7 +20,7 @@ export default function Topbar() {
           </div>
         </div>
         <div className="right">
-          <div className="hamburger">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
