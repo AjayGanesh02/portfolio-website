4774e281931c6d3870eeb47ac0f9e8f55c5d02d1
Ajay Ganesh <ajganesh2002@gmail.com>
2022-04-09 04:08:17 -0400

    Intro page complete

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,12 +1,38 @@
-import "./intro.scss"
+import "./intro.scss";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import { init } from 'ityped';
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+  const textRef = useRef();
+
+  useEffect(() => {
+    init(textRef.current,{
+      strings: ["Full Stack Developer", "Software Engineer", "Web Developer", "Mobile Developer"],
+      showCursor: false,
+      backdelay: 1500,
+      showCursor: true,
+      cursorChar: "|",
+    });
+  }, [])
   return (
     <div className="intro" id="intro">
       <div className="left">
-        <div className="imgContainer"></div>
+        <div className="imgContainer">
+          <img src="assets/ajay-ganesh.jpg" alt="Ajay Ganesh" />
+        </div>
       </div>
-      <div className="right"></div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi There, I'm</h2>
+          <h1>Ajay Ganesh</h1>
+          <h3>I'm a <span ref={textRef}></span></h3>
+        </div>
+        <a href="#portfolio">
+          <KeyboardArrowDownIcon className="icon" />
+        </a>
+      </div>
     </div>
   )
 }
